b1bc9d08e2d1c3b1df9db52773f7fb32149e6811
arcuar <nunnaerim@gmail.com>
2026-08-18 23:06:33 +0900

    feat(lecture): LectureService 코드 작성 (8.1·8.2·8.3)

--- src/main/java/com/getit/domain/lecture/repository/AssignmentRepository.java
+++ src/main/java/com/getit/domain/lecture/repository/AssignmentRepository.java
@@ -1,10 +1,13 @@
 package com.getit.domain.lecture.repository;
 
 import com.getit.domain.lecture.entity.Assignment;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 
   Optional<Assignment> findByLectureId(Long lectureId);
+
+  List<Assignment> findAllByLectureIdIn(List<Long> lectureIds);
 }
