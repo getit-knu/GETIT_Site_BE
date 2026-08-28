17d17b2964985bd66d17af3d12c65d49b938fbf0
arcuar <nunnaerim@gmail.com>
2026-08-28 20:14:47 +0900

    feat(setting): EventRequest 요청 DTO

---
+++ src/main/java/com/getit/domain/setting/event/dto/EventRequest.java
@@ -0,0 +1,17 @@
+package com.getit.domain.setting.event.dto;
+
+import com.getit.domain.setting.event.entity.EventType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
+public record EventRequest(
+    @NotNull Long generationId,
+    @NotBlank @Size(max = 100) String title,
+    @NotBlank @Size(max = 100) String place,
+    @NotNull LocalDate startDate,
+    @NotNull LocalDate endDate,
+    @NotNull EventType type,
+    @NotNull Boolean isVisible
+) { }
