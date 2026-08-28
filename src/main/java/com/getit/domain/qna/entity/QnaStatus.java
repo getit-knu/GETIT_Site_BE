5776e302619038e32bc5721358d03e7d06b9a9c2
arcuar <nunnaerim@gmail.com>
2026-08-28 23:56:42 +0900

    feat(qna): QnaStatus 라벨 + admin Q&A DTO

--- src/main/java/com/getit/domain/qna/entity/QnaStatus.java
+++ src/main/java/com/getit/domain/qna/entity/QnaStatus.java
@@ -1,6 +1,14 @@
 package com.getit.domain.qna.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum QnaStatus {
-  PENDING,
-  ANSWERED
+
+  PENDING("답변 대기"),
+  ANSWERED("답변 완료");
+
+  private final String label;
 }
