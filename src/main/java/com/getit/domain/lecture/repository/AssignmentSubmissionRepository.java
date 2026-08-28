7e2ce83fc60acee8086122ef8cf33350fc1c66cb
arcuar <nunnaerim@gmail.com>
2026-08-28 13:40:17 +0900

    feat(lecture): 과제별 제출물 목록 조회 메서드 추가

--- src/main/java/com/getit/domain/lecture/repository/AssignmentSubmissionRepository.java
+++ src/main/java/com/getit/domain/lecture/repository/AssignmentSubmissionRepository.java
@@ -1,10 +1,13 @@
 package com.getit.domain.lecture.repository;
 
 import com.getit.domain.lecture.entity.AssignmentSubmission;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssignmentSubmissionRepository extends JpaRepository<AssignmentSubmission, Long> {
 
   Optional<AssignmentSubmission> findByAssignmentIdAndUserId(Long assignmentId, Long userId);
+
+  List<AssignmentSubmission> findAllByAssignmentId(Long assignmentId);
 }
