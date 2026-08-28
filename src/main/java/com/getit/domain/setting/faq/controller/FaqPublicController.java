20e8730e195cceb3ac1f44ad0a607fb4fe98fdbf
arcuar <nunnaerim@gmail.com>
2026-08-29 06:16:25 +0900

    feat(setting): 공개 FAQ 목록 API (2.5)

---
+++ src/main/java/com/getit/domain/setting/faq/controller/FaqPublicController.java
@@ -0,0 +1,27 @@
+package com.getit.domain.setting.faq.controller;
+
+import com.getit.domain.setting.faq.service.FaqQueryService;
+import com.getit.domain.setting.faq.service.FaqView;
+import com.getit.global.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Public", description = "공개 사이트")
+@RestController
+@RequestMapping("/api/public/faqs")
+@RequiredArgsConstructor
+public class FaqPublicController {
+
+  private final FaqQueryService faqQueryService;
+
+  @Operation(summary = "FAQ 목록", description = "명세서 2.5")
+  @GetMapping
+  public ApiResponse<List<FaqView>> getFaqs() {
+    return ApiResponse.success(faqQueryService.findVisible());
+  }
+}
